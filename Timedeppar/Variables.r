094dfa5b0f06f4843c8f33d136f43d4eaebb156b
Alberto Saretto <albertosaretto99@gmail.com>
2023-05-04 19:12:17 +0200

    Timedeppar R

---
+++ Timedeppar/Variables.r
@@ -0,0 +1,18 @@
+
+#Variables
+
+# set colors 
+col_red <- '#E21B1B'   #red
+col_blue <- '#1769E6'  #blue
+col_green <- '#00D10A' #green
+col_grey <- '#C8CED0'  #gray
+
+freq <- 1/11
+dt <- 1
+
+# timedeppar settings 
+n_iter <- 10000
+n_interval <- 20
+n_adapt <- 2000
+
+n <- 500
